d34992d7d45cec3250973e53a2278c053a523d7e
CapitanCigarro <nigarcia2023@inf.udec.cl>
2024-07-02 19:25:32 -0400

    cambio grafico minimo

--- src/main/java/com/proyecto_semestral/Graphics/PanelPiso.java
+++ src/main/java/com/proyecto_semestral/Graphics/PanelPiso.java
@@ -2,40 +2,51 @@ package com.proyecto_semestral.Graphics;
 
 import java.awt.Color;
 
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
 import com.proyecto_semestral.Logic.GestorDeReservas;
 import com.proyecto_semestral.Logic.Piso;
 
-public class PanelPiso extends JPanel implements ActualizarColoresAsientos {
+public class PanelPiso extends JLayeredPane implements ActualizarColoresAsientos {
     private Piso piso;
     private PanelAsientosDerecha asientosDerecha;
     private PanelAsientosIzquierda asientosIzquierda;
     private int xSize = Sizes.PISO.getXSize();
     private int ySize = Sizes.PISO.getYSize();
+    private JLabel numPiso;
 
     public PanelPiso(Piso piso, GestorDeReservas g) {
         super();
         this.setSize(xSize, ySize);
         this.piso = piso;
 
+        this.numPiso = new JLabel();
+
+        this.numPiso.setSize(100, 30);
+        this.numPiso.setLocation(245, 650);
+        this.numPiso.setText("Piso = " + String.valueOf(piso.getnumPiso()));
+        this.numPiso.setBackground(Color.BLACK);
+        this.numPiso.setForeground(Color.WHITE);
+
         this.asientosDerecha = new PanelAsientosDerecha(piso.getFilaDerecha(), g);
         this.setLayout(null);
         this.asientosIzquierda = new PanelAsientosIzquierda(piso.getFilaIzquierda(), g);
-        this.asientosDerecha.setLocation(Sizes.PISO.getXSize() / 2, 0);
-
-        this.add(asientosIzquierda);
-        this.add(asientosDerecha);
-
-        System.out.println(asientosDerecha.getFilaAsientos().size() + ", " + asientosIzquierda.getFilaAsientos().size());
+        this.asientosDerecha.setLocation(Sizes.PISO.getXSize() / 2, 0);    
 
         if (piso.getnumPiso() == 1) {
             this.setColor(Color.DARK_GRAY);
 
         } else {
             this.setColor(Color.LIGHT_GRAY);
+            this.numPiso.setForeground(Color.BLACK);
 
         }
+
+        this.add(asientosIzquierda, JLayeredPane.DEFAULT_LAYER);
+        this.add(asientosDerecha, JLayeredPane.DEFAULT_LAYER);
+        this.add(numPiso, JLayeredPane.DRAG_LAYER);
 
     }
 
--- src/main/java/com/proyecto_semestral/Tests.java
+++ src/main/java/com/proyecto_semestral/Tests.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class Tests {
     @Test
     public void testing() {
-        ;
+        
 
     }
 
